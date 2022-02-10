e0f5c7d35455d9d8c024812e477493ad567fe6af
7ssSalha <husseinsoheil1999@hotmail.com>
2022-02-10 22:31:29 +0200

    Third Exercise - Quotient and Remainder

--- src/kotlinExercises/QuotientandRemainder.kt
+++ src/kotlinExercises/QuotientandRemainder.kt
@@ -1,2 +1,9 @@
 package kotlinExercises
 
+fun main(){
+    val num1 = 19
+    val num2 = 5
+    val quotient = num1 / num2
+    val remainder = num1 % num2
+    println("$num1 ÷ $num2 = $quotient and the remainder = $remainder")
+}
