6f49879fa1ce070815e86f0bac33dde8d67c61dd
7ssSalha <husseinsoheil1999@hotmail.com>
2022-02-11 17:01:27 +0200

    15th Exercise - Vowels & Consonants

--- src/kotlinExercises/VowelOrConsonant.kt
+++ src/kotlinExercises/VowelOrConsonant.kt
@@ -4,9 +4,14 @@ fun Char.isVowel():Boolean{
     return listOf('a','e','i','o','u').contains(this)
 }
 
+fun Char.isConsonant():Boolean{
+    return !listOf('a','e','i','o','u').contains(this) && this.isLetter()
+}
+
 fun main(){
     val a='a'
     val b='b'
     println(a.isVowel())
     println(b.isVowel())
+    println(b.isConsonant())
 }
